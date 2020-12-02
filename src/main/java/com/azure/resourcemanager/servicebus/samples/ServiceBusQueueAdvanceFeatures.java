7dc1d13f3da91876082e81de222189193ba8c81a
Azure-Fluent <azfluent@microsoft.com>
2020-12-02 14:02:13 +0800

    Release azure-resourcemanager 2.1.0

--- src/main/java/com/azure/resourcemanager/servicebus/samples/ServiceBusQueueAdvanceFeatures.java
+++ src/main/java/com/azure/resourcemanager/servicebus/samples/ServiceBusQueueAdvanceFeatures.java
@@ -1,30 +1,27 @@
-/**
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for
- * license information.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-package com.microsoft.azure.management.servicebus.samples;
+package com.azure.resourcemanager.servicebus.samples;
 
-import com.microsoft.azure.PagedList;
-import com.microsoft.azure.management.Azure;
-import com.microsoft.azure.management.resources.fluentcore.arm.Region;
-import com.microsoft.azure.management.resources.fluentcore.utils.SdkContext;
-import com.microsoft.azure.management.samples.Utils;
-import com.microsoft.azure.management.servicebus.AuthorizationKeys;
-import com.microsoft.azure.management.servicebus.NamespaceAuthorizationRule;
-import com.microsoft.azure.management.servicebus.NamespaceSku;
-import com.microsoft.azure.management.servicebus.Queue;
-import com.microsoft.azure.management.servicebus.ServiceBusNamespace;
-import com.microsoft.rest.LogLevel;
-import com.microsoft.windowsazure.Configuration;
-import com.microsoft.windowsazure.services.servicebus.ServiceBusConfiguration;
-import com.microsoft.windowsazure.services.servicebus.ServiceBusContract;
-import com.microsoft.windowsazure.services.servicebus.ServiceBusService;
-import com.microsoft.windowsazure.services.servicebus.models.BrokeredMessage;
-import org.joda.time.Period;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.http.rest.PagedIterable;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusMessage;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.core.management.Region;
+import com.azure.resourcemanager.samples.Utils;
+import com.azure.resourcemanager.servicebus.models.AuthorizationKeys;
+import com.azure.resourcemanager.servicebus.models.NamespaceAuthorizationRule;
+import com.azure.resourcemanager.servicebus.models.NamespaceSku;
+import com.azure.resourcemanager.servicebus.models.Queue;
+import com.azure.resourcemanager.servicebus.models.ServiceBusNamespace;
 
-import java.io.File;
+import java.time.Duration;
 
 /**
  * Azure Service Bus basic scenario sample.
@@ -46,15 +43,15 @@ public final class ServiceBusQueueAdvanceFeatures {
 
     /**
      * Main function which runs the actual sample.
-     * @param azure instance of the azure client
+     * @param azureResourceManager instance of the azure client
      * @return true if sample runs successfully
      */
-    public static boolean runSample(Azure azure) {
+    public static boolean runSample(AzureResourceManager azureResourceManager) {
         // New resources
-        final String rgName = SdkContext.randomResourceName("rgSB04_", 24);
-        final String namespaceName = SdkContext.randomResourceName("namespace", 20);
-        final String queue1Name = SdkContext.randomResourceName("queue1_", 24);
-        final String queue2Name = SdkContext.randomResourceName("queue2_", 24);
+        final String rgName = Utils.randomResourceName(azureResourceManager, "rgSB04_", 24);
+        final String namespaceName = Utils.randomResourceName(azureResourceManager, "namespace", 20);
+        final String queue1Name = Utils.randomResourceName(azureResourceManager, "queue1_", 24);
+        final String queue2Name = Utils.randomResourceName(azureResourceManager, "queue2_", 24);
         final String sendRuleName = "SendRule";
 
         try {
@@ -63,12 +60,12 @@ public final class ServiceBusQueueAdvanceFeatures {
 
             System.out.println("Creating name space " + namespaceName + " in resource group " + rgName + "...");
 
-            ServiceBusNamespace serviceBusNamespace = azure.serviceBusNamespaces()
-                    .define(namespaceName)
-                    .withRegion(Region.US_WEST)
-                    .withNewResourceGroup(rgName)
-                    .withSku(NamespaceSku.STANDARD)
-                    .create();
+            ServiceBusNamespace serviceBusNamespace = azureResourceManager.serviceBusNamespaces()
+                .define(namespaceName)
+                .withRegion(Region.US_WEST)
+                .withNewResourceGroup(rgName)
+                .withSku(NamespaceSku.STANDARD)
+                .create();
 
             System.out.println("Created service bus " + serviceBusNamespace.name());
             Utils.print(serviceBusNamespace);
@@ -78,11 +75,11 @@ public final class ServiceBusQueueAdvanceFeatures {
             System.out.println("Creating first queue " + queue1Name + ", with session, time to live and move to dead-letter queue features...");
 
             Queue firstQueue = serviceBusNamespace.queues().define(queue1Name)
-                    .withSession()
-                    .withDefaultMessageTTL(new Period().withMinutes(10))
-                    .withExpiredMessageMovedToDeadLetterQueue()
-                    .withMessageMovedToDeadLetterQueueOnMaxDeliveryCount(40)
-                    .create();
+                .withSession()
+                .withDefaultMessageTTL(Duration.ofMinutes(10))
+                .withExpiredMessageMovedToDeadLetterQueue()
+                .withMessageMovedToDeadLetterQueueOnMaxDeliveryCount(40)
+                .create();
             Utils.print(firstQueue);
 
             //============================================================
@@ -91,10 +88,10 @@ public final class ServiceBusQueueAdvanceFeatures {
             System.out.println("Creating second queue " + queue2Name + ", with De-duplication and AutoDeleteOnIdle features...");
 
             Queue secondQueue = serviceBusNamespace.queues().define(queue2Name)
-                    .withSizeInMB(2048)
-                    .withDuplicateMessageDetection(new Period().withMinutes(10))
-                    .withDeleteOnIdleDurationInMinutes(10)
-                    .create();
+                .withSizeInMB(2048)
+                .withDuplicateMessageDetection(Duration.ofMinutes(10))
+                .withDeleteOnIdleDurationInMinutes(10)
+                .create();
 
             System.out.println("Created second queue in namespace");
 
@@ -104,8 +101,8 @@ public final class ServiceBusQueueAdvanceFeatures {
             // Update second queue to change time for AutoDeleteOnIdle.
 
             secondQueue = secondQueue.update()
-                    .withDeleteOnIdleDurationInMinutes(5)
-                    .apply();
+                .withDeleteOnIdleDurationInMinutes(5)
+                .apply();
 
             System.out.println("Updated second queue to change its auto deletion time");
 
@@ -114,9 +111,9 @@ public final class ServiceBusQueueAdvanceFeatures {
             //=============================================================
             // Update first queue to disable dead-letter forwarding and with new Send authorization rule
             secondQueue = firstQueue.update()
-                    .withoutExpiredMessageMovedToDeadLetterQueue()
-                    .withNewSendRule(sendRuleName)
-                    .apply();
+                .withoutExpiredMessageMovedToDeadLetterQueue()
+                .withNewSendRule(sendRuleName)
+                .apply();
 
             System.out.println("Updated first queue to change dead-letter forwarding");
 
@@ -125,8 +122,8 @@ public final class ServiceBusQueueAdvanceFeatures {
             //=============================================================
             // Get connection string for default authorization rule of namespace
 
-            PagedList<NamespaceAuthorizationRule> namespaceAuthorizationRules = serviceBusNamespace.authorizationRules().list();
-            System.out.println("Number of authorization rule for namespace :" + namespaceAuthorizationRules.size());
+            PagedIterable<NamespaceAuthorizationRule> namespaceAuthorizationRules = serviceBusNamespace.authorizationRules().list();
+            System.out.println("Number of authorization rule for namespace :" + Utils.getSize(namespaceAuthorizationRules));
 
 
             for (NamespaceAuthorizationRule namespaceAuthorizationRule: namespaceAuthorizationRules) {
@@ -135,7 +132,7 @@ public final class ServiceBusQueueAdvanceFeatures {
 
             System.out.println("Getting keys for authorization rule ...");
 
-            AuthorizationKeys keys = namespaceAuthorizationRules.get(0).getKeys();
+            AuthorizationKeys keys = namespaceAuthorizationRules.iterator().next().getKeys();
             Utils.print(keys);
 
             //=============================================================
@@ -144,17 +141,13 @@ public final class ServiceBusQueueAdvanceFeatures {
 
             //=============================================================
             // Send a message to queue.
-
-            try {
-                Configuration config = Configuration.load();
-                config.setProperty(ServiceBusConfiguration.CONNECTION_STRING, keys.primaryConnectionString());
-                ServiceBusContract service = ServiceBusService.create(config);
-                BrokeredMessage message = new BrokeredMessage("Hello");
-                message.setSessionId("23424");
-                service.sendQueueMessage(queue1Name, message);
-            }
-            catch (Exception ex) {
-            }
+            ServiceBusSenderClient sender = new ServiceBusClientBuilder()
+                .connectionString(keys.primaryConnectionString())
+                .sender()
+                .queueName(queue1Name)
+                .buildClient();
+            sender.sendMessage(new ServiceBusMessage("Hello").setSessionId("23424"));
+            sender.close();
 
             //=============================================================
             // Delete a queue and namespace
@@ -164,21 +157,14 @@ public final class ServiceBusQueueAdvanceFeatures {
 
             System.out.println("Deleting namespace " + namespaceName + "...");
             // This will delete the namespace and queue within it.
-            try {
-                azure.serviceBusNamespaces().deleteById(serviceBusNamespace.id());
-            }
-            catch (Exception ex) {
-            }
+            azureResourceManager.serviceBusNamespaces().deleteById(serviceBusNamespace.id());
             System.out.println("Deleted namespace " + namespaceName + "...");
 
             return true;
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            e.printStackTrace();
         } finally {
             try {
                 System.out.println("Deleting Resource Group: " + rgName);
-                azure.resourceGroups().beginDeleteByName(rgName);
+                azureResourceManager.resourceGroups().beginDeleteByName(rgName);
                 System.out.println("Deleted Resource Group: " + rgName);
             } catch (NullPointerException npe) {
                 System.out.println("Did not create any resources in Azure. No clean up is necessary");
@@ -186,31 +172,30 @@ public final class ServiceBusQueueAdvanceFeatures {
                 g.printStackTrace();
             }
         }
-        return false;
     }
 
     /**
      * Main entry point.
+     *
      * @param args the parameters
      */
     public static void main(String[] args) {
         try {
+            final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+            final TokenCredential credential = new DefaultAzureCredentialBuilder()
+                .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
+                .build();
 
-            //=============================================================
-            // Authenticate
-
-            final File credFile = new File(System.getenv("AZURE_AUTH_LOCATION"));
-
-            Azure azure = Azure
-                    .configure()
-                    .withLogLevel(LogLevel.BODY_AND_HEADERS)
-                    .authenticate(credFile)
-                    .withDefaultSubscription();
+            AzureResourceManager azureResourceManager = AzureResourceManager
+                .configure()
+                .withLogLevel(HttpLogDetailLevel.BASIC)
+                .authenticate(credential, profile)
+                .withDefaultSubscription();
 
             // Print selected subscription
-            System.out.println("Selected subscription: " + azure.subscriptionId());
-            runSample(azure);
+            System.out.println("Selected subscription: " + azureResourceManager.subscriptionId());
 
+            runSample(azureResourceManager);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
